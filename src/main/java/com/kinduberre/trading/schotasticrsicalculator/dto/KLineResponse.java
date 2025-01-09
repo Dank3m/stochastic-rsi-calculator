716a9953a163d5f2807fc5a85336384d5a115556
Dank3m <dankimist@gmail.com>
2025-01-09 14:32:50 +0300

    Revert "Initial Commit"
    This reverts commit 505fbbb781ea28080630e84c028600c51b802bfe.

--- src/main/java/com/kinduberre/trading/schotasticrsicalculator/dto/KLineResponse.java
+++
@@ -1,7 +0,0 @@
-package com.kinduberre.trading.schotasticrsicalculator.dto;
-
-import java.util.List;
-
-public class KLineResponse {
-    List<List<Object>> KLineResponse;
-}
--- src/main/java/com/kinduberre/trading/schotasticrsicalculator/service/BinanceService.java
+++
@@ -1,9 +0,0 @@
-package com.kinduberre.trading.schotasticrsicalculator.service;
-
-import java.io.IOException;
-import java.util.List;
-
-public interface BinanceService {
-
-    public List<List<Object>> getKlines(String symbol, String interval, Integer limit) throws IOException;
-}
